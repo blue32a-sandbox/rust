010fff111f88ba6691b248c59d0e23fc35b7366c
blue32a <blue32a.jp@gmail.com>
2022-01-30 23:23:43 +0900

    Control Flow

---
+++ src/ch03/05-control-flow/while.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    println!();
+
+    // Looping Through a Collection with for
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
