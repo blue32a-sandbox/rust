bdc77fdfe126b370324a26e2a34867113b419a2e
blue32a <blue32a.jp@gmail.com>
2022-03-27 15:25:36 +0900

    All the Places Patterns Can Be Used

---
+++ src/ch18/01-all-the-places-for-patterns/function.rs
@@ -0,0 +1,9 @@
+// 引数はパターン
+fn print_coordinate(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
+fn main() {
+    let point = (3, 5);
+    print_coordinate(&point);
+}
